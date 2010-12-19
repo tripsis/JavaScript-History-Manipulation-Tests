780574547824a3545c57ee578cd6747fd9843d06
Kev <kwilson@ibigroup.com>
2010-12-19 01:44:09 +0000

    Added support for back button in browser (although still not got support for page titles in history).

--- Website/Scripts/jquery.history.js
+++ Website/Scripts/jquery.history.js
@@ -3,8 +3,11 @@
     this.init = function (links) {
 
         // Check for support
-        if (window.history && window.history.replaceState) {
+        if (window.history && window.history.pushState) {
             _overrideClickEvents(links);
+            $(window).bind("popstate", function (b) {
+                _popStateHandler(b.originalEvent)
+            });
         }
         else {
             alert("not supported");
@@ -19,7 +22,7 @@
             var url = $(this).attr("href");
 
             // Load the content into the container on the current page
-            $("#main").load(url + " #main", function () {
+            _loadPageContent(url, function () {
                 // Update the URL of the page.
                 _setPageUrl(url);
             });
@@ -29,8 +32,26 @@
 
     };
 
+    var _loadPageContent = function (url, successCallback) {
+        // Load the #main content from the target page into this page
+        $("#main").load(url + " #main", function () {
+            if (successCallback) {
+                successCallback(url);
+            }
+        });
+    };
+
     var _setPageUrl = function (url) {
-        window.history.replaceState(null, null, url);
+        window.history.pushState(url, url, url);
+    };
+
+    var _popStateHandler = function (event) {
+        if (event.state) {
+            var url = event.state;
+            if (url) {
+                _loadPageContent(url);
+            }
+        }
     };
 
 };
