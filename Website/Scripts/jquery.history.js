452e02c9e319a7ab8bfaf11c57e6bcb5dc26b082
Kev <kwilson@ibigroup.com>
2010-12-19 12:03:30 +0000

    Added (inefficient - see comments) code to load page title from pages. Updated notes throughout JS.

--- Website/Scripts/jquery.history.js
+++ Website/Scripts/jquery.history.js
@@ -4,13 +4,22 @@
 
         // Check for support
         if (window.history && window.history.pushState) {
+
+            // Set click event handlers
             _overrideClickEvents(links);
+
+            // Store the current page in history so that if you go
+            // back far enough to the original page, it has a valid
+            // object state.
+            _storeCurrentPage();
+
+            // Bind popstate for handling history traversal
             $(window).bind("popstate", function (b) {
                 _popStateHandler(b.originalEvent)
             });
         }
         else {
-            alert("not supported");
+            alert("window.history.pushState is not supported in this browser");
         }
     };
 
@@ -33,15 +42,43 @@
     };
 
     var _loadPageContent = function (url, successCallback) {
+
+        // ==============================================================
+        // NOTE
+        // ==============================================================
+        // Obviously, you wouldn't do things this way in production as
+        // this method loads the page twice using AJAX, once for the 
+        // content then again for the title.
+        // 
+        // But, this is just a sample of how the history push works so
+        // it's not worth refining at the moment. In a production
+        // environment, you'd likely be loading page content from a DB
+        // so could return the title and content as part of a JSON object.
+        // ==============================================================
+
         // Load the #main content from the target page into this page
         $("#main").load(url + " #main", function () {
 
-            $("title").text("Changed"); // placeholder until I work out how to load this with jQuery.
+            // Load the page title
+            $.get(url, function (response) {
+
+                // A little hacky; this will fail (on Chrome at least) if there's
+                // a line break in the <title> tag.
+                var title = (/<title>(.*?)<\/title>/m).exec(response)[1];
+                $("title").text(title);
+            });
 
             if (successCallback) {
                 successCallback(url);
             }
+
         });
+    };
+
+    var _storeCurrentPage = function () {
+        // Stores the current page in the window history.
+        var url = window.location.pathname;
+        window.history.replaceState(url, url, url);
     };
 
     var _setPageUrl = function (url) {
@@ -49,12 +86,14 @@
         //
         // In this example, we're just adding the URL to the state object but we could
         // add the entire page content if we wanted (although this could cause the memory
-        // to grow very quickly and wouldn't update with changes to the page on the server.
+        // to grow very quickly and wouldn't update with changes to the page on the server).
         window.history.pushState(url, url, url);
     };
 
+    // Handles the user moving through browser history.
     var _popStateHandler = function (event) {
         if (event.state) {
+            // event.state is the first parameter added to the pushState's State Object (in this case, a URL).
             var url = event.state;
             if (url) {
                 _loadPageContent(url);
