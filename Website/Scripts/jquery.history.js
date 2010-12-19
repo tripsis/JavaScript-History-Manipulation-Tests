9f53a65b1b6d08bff718658b5102e85a55b78304
Kev <kwilson@ibigroup.com>
2010-12-19 01:06:24 +0000

    Initial commit of basic test code.

---
+++ Website/Scripts/jquery.history.js
@@ -0,0 +1,38 @@
+﻿var jqueryHistoryHelper = function () {
+
+    this.init = function (links) {
+
+        // Check for support
+        if (window.history && window.history.replaceState) {
+            _overrideClickEvents(links);
+        }
+        else {
+            alert("not supported");
+        }
+    };
+
+    var _overrideClickEvents = function (links) {
+
+        links.click(function () {
+
+            // Get the URL of whatever we were clicking on
+            var url = $(this).attr("href");
+
+            // Load the content into the container on the current page
+            $("#main").load(url + " #main", function () {
+                // Update the URL of the page.
+                _setPageUrl(url);
+            });
+
+            return false;
+        });
+
+    };
+
+    var _setPageUrl = function (url) {
+        window.history.replaceState(null, null, url);
+    };
+
+};
+
+var historyHelper = new jqueryHistoryHelper();
---
+++ Website/Views/Home/About.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage<dynamic>" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+	About
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h1>About</h1>
+
+    <p>rendered <%: DateTime.Now.ToString() %></p>
+
+</asp:Content>
